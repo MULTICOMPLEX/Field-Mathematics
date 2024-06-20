c18466a17df46448af76f7c2e438fa1b66355d33
MULTICOMPLEX <75379917+MULTICOMPLEX@users.noreply.github.com>
2024-06-20 22:19:24 +0200

    update.

--- Neuron_Fourier_2.py
+++ Neuron_Fourier_2.py
@@ -25,16 +25,16 @@ def elu(x, alpha=1.0):
 def swish(x):
     return x * sigmoid(x)
     
-def Fourier_transform(z):
-    return np.fft.fft(z).real
+def IFFT(z):
+    return np.fft.ifft(z).real
 
-def Fourier_transform_derivative(f):
-    f = np.fft.ifft(1j*np.fft.fft(f))
+def  FFT(f):
+    f = np.fft.fft(f)
     return f.real 
     
 # Function to calculate the output of the neuron
 def neuron_output(inputs, bias):
-    output = Fourier_transform(inputs + bias)
+    output = IFFT(inputs + bias)
     return output
 
 # Function to update the weights and bias using backpropagation
@@ -42,7 +42,7 @@ def update_bias(inputs, bias, target_output, learning_rate):
 
     # Calculate the neuron's output
     output = neuron_output(inputs, bias)
-    fft =  Fourier_transform_derivative(output)
+    fft =  FFT(output)
     
     for i in range(len(target_output)):
     # Calculate the error (difference between the target output and the actual output)
@@ -55,34 +55,35 @@ def update_bias(inputs, bias, target_output, learning_rate):
     return bias
 
 # Define the initial input values (example input features) and target output for training
-training_inputs = np.array([[0.5, 0.7], [0.8, 0.2], [0.3, 0.9]])
-training_target_output = np.array([0.9, 0.7, 0.4])
+training_inputs = np.array([[0.5, 0.7, 0.77], [0.8, 0.2, 0.432], [0.3, 0.9, 0.6754], [0.8, 0.1, 0.55543], [0.334, 0.6, 0.55543], [0.334, 0.1, 0.55543]])
+training_target = np.array([0.9, 0.7, 0.4, 0.56, 0.7, 0.234])
 
 # Define new input values for testing (example new input features)
-new_inputs = np.array([0.8, 0.2])
-new_inputs = training_inputs
-
+new_inputs = np.array([0.334, 0.1, 0.55543])
+#new_inputs = training_inputs
 
 # Define the bias term (initially random or zero)
-bias = np.array([[0.0, 0.0],[0.0, 0.0],[0.0, 0.0]])
-# Define the learning rate (how quickly the model should learn)
-learning_rate = 0.3
+bias = np.zeros_like(training_inputs)
 
-iterations = 300
+# Define the number of iterations
+iterations = 200
+
+# Define the learning rate (how quickly the model should learn)
+learning_rate = 4
 
 for _ in range(iterations):
-    bias = update_bias(training_inputs, bias, training_target_output, learning_rate)
+    bias = update_bias(training_inputs, bias, training_target, learning_rate)
+
+print("Iterations:", iterations)
+print("Learning rate:", learning_rate)
+# Print the training_target
+print("Training target: ", training_target)
 
 # Calculate the output of the neuron with the new input data
 new_output = neuron_output(new_inputs, bias)
 
-# Print the final weights, bias, and output with the new input data
-print("Iterations:", iterations)
-#print("Final bias:", bias)
-print("Training target output:", training_target_output)
-
 first_column = []
 for row in new_output:
     first_column.append(row[0])  # Extract the first element from each row
-print("Output of the neuron: ", first_column) 
+print("Neuron output: ", first_column) 
 #print("Output of the neuron: ", new_output)
